c21c24bb635720bd9860b3220b1b7485706608c0
Nans <91684269+Narvak@users.noreply.github.com>
2023-10-06 17:16:15 +0200

    Add files via upload

--- src/webservices/MVC/Model/MessageModel.java
+++ src/webservices/MVC/Model/MessageModel.java
@@ -1,12 +1,53 @@
 package webservices.MVC.Model;
 
 public class MessageModel {
+	
+	private String name;
+	private String phoneNumber;
+	private String mail;
+	private String society;
 	private String message;
 	
-	public Message(String message) {
+	public Message(String name, String phoneNumber, String mail, String society, String message) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.mail = mail;
+		this.society = society;
 		this.message = message;
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getSociety() {
+		return society;
+	}
+
+	public void setSociety(String society) {
+		this.society = society;
+	}
+
 	public String getMessage() {
 		return message;
 	}
