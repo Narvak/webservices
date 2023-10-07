98e4814976abf3ffe1db2c2a1f5233eff067d898
Nans <91684269+Narvak@users.noreply.github.com>
2023-10-07 16:57:05 +0200

    Add files via upload

--- src/webservices/Main.java
+++ src/webservices/Main.java
@@ -3,12 +3,9 @@ package webservices;
 public class Main {
 	
 	public static void main(String args[]) {
-		System.out.println("Bloc statique");
+		System.out.println("Start.");
+		System.out.println("Webservice is open.");
+		System.out.println("");
+		System.out.println("");
 	}
-	
-	
-	
-	
-	
-	
 }
